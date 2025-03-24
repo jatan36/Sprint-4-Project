4cf59d2374279049b05f12f6cf752d7bd6888a0e
jatan36 <jatan.bhatt@proton.me>
2025-03-24 18:29:02 +0000

    Updated Try

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ import plotly.express as px
 
 df = pd.read_csv("vehicles_us.csv")
 
+df_1 = df.groupby('type')['price'].mean().reset_index()
+df_1 = df_1.sort_values(by='price')
+
+df_2 = df.groupby(['manufacturer', 'condition'])['odometer'].mean().reset_index()
+
+df_3 = df['paint_color'].value_counts().reset_index()
+df_3.columns = ['paint_color', 'count']
+
 st.header("Vehicle Sales Analysis")
 
 fig = px.histogram(df_1, x="type", y = 'price', color = 'price', title="Average Price per Type of Vehicle")
